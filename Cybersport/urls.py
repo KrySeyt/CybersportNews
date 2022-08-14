566080904231bb6f4cbfc359a43c3b7d5359c8a9
KrySeyt <kryseyt@gmail.com>
2022-08-14 18:19:14 +0300

    Added redirect from "" to main-page

--- Cybersport/urls.py
+++ Cybersport/urls.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.urls import path
 
 from . import views
@@ -15,7 +16,8 @@ urlpatterns = [
     path('authorization/', views.authorization, name='authorization'),
     path('logout/', views.logout_user, name='logout'),
     path('search/', views.search, name='search'),
-    path('email-confirmation/sended', views.email_confirmation_sended, name='email-confirmation-sended'),
+    path('email-confirmation/sended/', views.email_confirmation_sended, name='email-confirmation-sended'),
     path('email-confirmation/', views.email_confirmation, name='email-confirmation'),
     path('test', views.test),
+    path('', lambda request: redirect('main-page')),
 ]
